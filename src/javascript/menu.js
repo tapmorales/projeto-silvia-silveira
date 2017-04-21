53dd820731403b81fcf674b5a32389027111d0da
Silvia Silveira <silvia@silveira.com>
2017-04-21 15:07:26 -0300

    finalizacao do menu

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -3,8 +3,18 @@
     
     var btn = document.querySelector('.header-nav__hamburgger');
     var html = document.querySelector('html');
+    var menu = document.querySelector('#mainMenu');
+    var classMenu = 'menu-opened';
     var menuOpened = false;
     
+    html.addEventListener('click', function(e){
+        console.log(this);
+        console.log(e.target);
+        
+        if(e.target === html && menuOpened){
+            closeMenu();
+        }
+    })
     
     btn.addEventListener('click', toggleMenu);
     
@@ -18,12 +28,19 @@
     
     function closeMenu(){
         menuOpened = false;
-        html.classList.remove('menu-opened');
-        
+        html.classList.remove(classMenu);
+        btn.blur();
+        menu.setAttribute('aria-expanded', false);
+        btn.setAttribute('aria-expanded', false);
     }
     
     function openMenu(){
         menuOpened = true;
-        html.classList.add('menu-opened');
+        html.classList.add(classMenu);
+        menu.setAttribute('aria-expanded', true);
+        btn.setAttribute('aria-expanded', true);
     }
 }())
+
+
+
