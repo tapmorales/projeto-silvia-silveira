e3f13f4849c35c500e9cce1a58fc8d19d26c6dd6
Silvia Silveira <silvia@silveira.com>
2017-06-27 19:43:07 -0300

    finalizacao projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,85 @@
 var gulp    = require('gulp')
-    , sass  = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    , sass  = require('gulp-sass')
+    , include = require('gulp-file-include')
+    , clean = require('gulp-clean')
+    , autoprefixer = require('gulp-autoprefixer')
+    , uncss = require('gulp-uncss')
+    , imagemin = require('gulp-imagemin')
+    , cssnano = require('gulp-cssnano')
+    , uglify = require('gulp-uglify')
+    , concat = require('gulp-concat')
+    , browserSync = require('browser-sync');
 
 
-gulp.task('sass', function(){
-    gulp.src('./src/sass/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./src/css/'));
+gulp.task('clean', function(){
+    return gulp.src('dist')
+        .pipe(clean());
 })
 
+gulp.task('copy', ['clean'], function(){
+    return gulp.src([
+        'src/components/bootstrap/dist/**/*', 
+        'src/components/bootstrap/fonts/**/*', 
+        'src/components/bootstrap/js/**/*', 
+        'src/components/font-awesome/css/**/*', 
+        'src/components/font-awesome/fonts/**/*'], {"base": "src"})
+        .pipe(gulp.dest('dist'))
+})
 
+gulp.task('sass', function(){
+    return gulp.src('./src/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(cssnano())
+        .pipe(gulp.dest('./dist/css/'));
+})
 
+gulp.task('html', function(){
+    return gulp.src([
+            './src/**/*.html',
+            '!src/inc/**'
+        ])
+        .pipe(include())
+        .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('uncss', ['html'], function(){
+    return gulp.src('./dist/components/**/*.css')
+        .pipe(uncss({
+            html: ['./dist/*.html']
+        }))
+        .pipe(gulp.dest('./dist/components/'))
+})
+
+gulp.task('imagemin', function(){
+    return gulp.src('./src/imagens/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/imagens/'))
+})
+
+gulp.task('build-js', function(){
+    return gulp.src('src/javascript/**/*')
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/javascript/'))
+})
+
+gulp.task('default', ['copy'], function(){
+    gulp.start('uncss', 'imagemin', 'sass', 'build-js')
+})
 
 gulp.task('server', function(){
+    
     browserSync.init({
         server: {
-            baseDir: 'src'
+            baseDir: 'dist'
         }
     })
     
-    gulp.watch('./src/**/*').on('change', browserSync.reload)
+    gulp.watch('./dist/**/*').on('change', browserSync.reload)
     
     gulp.watch('./src/sass/**/*.scss', ['sass'])
+    gulp.watch('./src/**/*.html', ['html'])
+    gulp.watch('./src/javascript/**/*', ['build-js'])
 })
+
